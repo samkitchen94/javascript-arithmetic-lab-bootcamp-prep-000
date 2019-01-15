3e75db09bfd7d95a2fbdf807d82e2455dea2ecfc
Sam Kitchen <samkitchen94@gmail.com>
2019-01-15 17:51:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,27 +11,11 @@ subtract(60, 40)
 function multiply(x, y) {
   return (x* y)
 }
-
 multiply(2, 3.4)
 
 function divide(x, y) {
   return x/ y
 }
-  
   divide(5.0,2.5)
 
-function inc(n) {
- return n+1
-}
 
-inc(n)
-
-function dec(n) {
-  return n-1
-}
-
-dec(n)
-
-function makeInt(n) {
-  return 
-}
